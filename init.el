73b214c7690819e544d80e60748cee680165ea4e
Aiden Taylor <91446658+act17@users.noreply.github.com>
2022-07-30 23:01:26 -0600

    Initial Commit

---
+++ init.el
@@ -0,0 +1,44 @@
+;;An attempt at an EMacs Config                                                                                                               
+;;Made by act, July 28th, 2022                                                                                                                
+;;Released on July 30th, 2022                                                                                                                 
+;;Under the GNU GPL 3.0                                                                                                                       
+;;github.com/act17/actemacs                                                                                                                   
+                                                                                                                                              
+;;Enabling numbers for buffers:                                                                                                               
+(global-display-line-numbers-mode 1)                                                                                                          
+                                                                                                                                              
+;;Creating an... operator? Value?                                                                                                             
+(setq lines 1)                                                                                                                                
+                                                                                                                                              
+;;Setting a Theme                                                                                                                             
+(load-theme 'deeper-blue t)                                                                                                                   
+                                                                                                                                              
+;;Key-Binding                                                                                                                                 
+(define-key global-map (kbd "C-x c") 'act/working-directory)                                                                                  
+(define-key global-map (kbd "C-x M-l") 'act/lineswitch)                                                                                       
+                                                                                                                                              
+;;Creating some sort of command                                                                                                               
+(defun act/working-directory ()                                                                                                               
+  "Goes to the working directory.";;This is for my working directory.                                                                         
+  (interactive)                                                                                                                               
+  (dired "~/c/")                                                                                                                              
+)                                                                                                                                             
+                                                                                                                                              
+;;This switches lines on and off.                                                                                                             
+(defun act/lineswitch ()                                                                                                                      
+  "Turns lines on/off"                                                                                                                        
+  (interactive)                                                                                                                               
+  (if (= lines 0) (act/lineon) (act/lineoff) )                                                                                                
+)
+
+(defun act/lineon ()                                                                                                                          
+  (global-display-line-numbers-mode 1)                                                                                                        
+  (setq lines 1)                                                                                                                              
+  (message "Lines are turned on.")                                                                                                            
+)                                                                                                                                             
+                                                                                                                                              
+(defun act/lineoff ()                                                                                                                         
+  (global-display-line-numbers-mode 0)                                                                                                        
+  (setq lines 0)                                                                                                                              
+  (message "Lines are turned off.")                                                                                                           
+)                                                                                                                                             
